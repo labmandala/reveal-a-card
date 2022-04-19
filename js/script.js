23dc7a68a1155464a0f206de1c0a96e38dfcee40
sarah <skg@neofilm.us>
2022-04-19 11:14:45 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e);
+  //console.log(e.key);
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "🐚";
+  } else if (key === "2") {
+    box2.innerText = "🍄";
+  } else if (key === "3") {
+    box3.innerText = "🦄";
+  }
+});
+
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
